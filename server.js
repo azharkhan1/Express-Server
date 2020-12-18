50c7db9996c2f1a41d2813cc96f2acb4cc16d3db
Muhammad <azhar40@live.co.uk>
2020-12-18 11:43:58 +0500

    Check

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.get("/",(req,res,next)=>{
     res.send("Hello, I am here to serve you");
 });
 
-server.listen("process.env.PORT" ||3000,()=>{
+server.listen(process.env.PORT ||3000,()=>{
 
     console.log("server is running on port 3000");
 });
