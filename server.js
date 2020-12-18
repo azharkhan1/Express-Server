d998d98f1ad488c266d6a34e7d0ad8048309483a
Muhammad <azhar40@live.co.uk>
2020-12-18 11:15:25 +0500

    done with server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+let server = express();
+
+server.get("/",(req,res,next)=>{
+
+    res.send("Hello, I am here to serve you");
+});
+
+server.listen(3000,()=>{
+
+    console.log("server is running on port 3000");
+});
